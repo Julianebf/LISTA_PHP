d55e11b17023ea8e3cb17119b609cf6a39e8a0a8
Juliane Bezerra <fbjuliane6@gmail.com>
2022-04-14 19:46:28 -0300

    inform conditional

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
     $primeiroValor=$_POST['valor1'];
     $segundoValor=$_POST['valor2'];
     $soma = $primeiroValor + $segundoValor;
+
+    if ($soma > 20){
+        $total = $soma + 8;
+    }
+    else{
+        $total = $soma - 5;
+    }
 ?>
