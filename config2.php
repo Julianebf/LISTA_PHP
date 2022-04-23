bd60352ce24f23586ece95ef2ed2c58dda5a086e
Juliane Bezerra <fbjuliane6@gmail.com>
2022-04-23 18:18:38 -0300

    start backend

--- config2.php
+++ config2.php
@@ -1,13 +1,3 @@
 <?php
-function divisible($number){
-    if($number % 10 == 0) {
-        echo "$number O numero é divisivel por 10!";
-    }
-    else if($number % 5 == 0){
-        echo "$number O numero é divisivel por 5!";
-    }
-    else if($number % 2 == 0)
-        echo "$number O numero é divisivel por 2!";
-    }
-    return divisible($number)
+
 ?>
