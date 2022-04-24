cc2726de8868815a78fbc571dcd6fe0357510ca5
Juliane Bezerra <fbjuliane6@gmail.com>
2022-04-24 13:27:29 -0300

    create conditional

--- config.php
+++ config.php
@@ -1,4 +1,16 @@
 <?php
     function divisible($number){
+        if($number % 10 == 0) {
+            echo "$number  is divisible by 10!";
+        }
+        else if($number % 5 == 0){
+            echo "$number  is divisible by 5!";
+        }
+        else if($number % 2 == 0){
+            echo "$number  is divisible by 2!";
+        }
+        else {
+            echo "$number  not's divisible by 10 by 5 or by 2!";
+        }
     }
 ?>
