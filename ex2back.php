6e6db2d2bf2a08c69fd74a9a5b241fe377dfba54
Juliane Bezerra <fbjuliane6@gmail.com>
2022-04-27 16:56:56 -0300

    add logical preposition

--- ex2back.php
+++ ex2back.php
@@ -1,5 +1,17 @@
 <?php
 function divisible($number){
+    if ($number % 10 == 0 && $number % 5 == 0 && $number % 2 == 0){
+        echo "$number is divisible by 10, 5 and 2!";
+    }
+    else if ($number % 10 == 0 && $number % 5 == 0){
+        echo "$number is divisible by 10 and 5!";
+    }
+    else if ($number % 10 == 0 && $number % 2 == 0){
+        echo "$number is divisible by 10 and 2!";
+    }
+    else if ($number % 5 == 0 && $number % 2 == 0){
+        echo "$number is divisible by 5 and 2!";
+    }  
     if($number % 10 == 0) {
         echo "$number  is divisible by 10!";
     }
