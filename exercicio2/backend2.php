fda95d507e64bd814277a9cf45be589518638a4d
Juliane Bezerra <fbjuliane6@gmail.com>
2022-05-01 21:18:33 -0300

    add folder exercicio2

---
+++ exercicio2/backend2.php
@@ -0,0 +1,28 @@
+<?php
+function divisible($number){
+    if ($number % 10 == 0 && $number % 5 == 0 && $number % 2 == 0){
+        echo "$number is divisible by 10, 5 and 2!";
+    }
+    else if ($number % 10 == 0 && $number % 5 == 0){
+        echo "$number is divisible by 10 and 5!";
+    }
+    else if ($number % 10 == 0 && $number % 2 == 0){
+        echo "$number is divisible by 10 and 2!";
+    }
+    else if ($number % 5 == 0 && $number % 2 == 0){
+        echo "$number is divisible by 5 and 2!";
+    }  
+    if($number % 10 == 0) {
+        echo "$number  is divisible by 10!";
+    }
+    else if($number % 5 == 0){
+        echo "$number  is divisible by 5!";
+    }
+    else if($number % 2 == 0){
+        echo "$number  is divisible by 2!";
+    }
+    else {
+        echo "$number  not's divisible by 10 by 5 or by 2!";
+    }
+}
+?>
