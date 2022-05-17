42d80d1613b59c5478654edf3a67a5bb6c9800f4
Juliane Bezerra <fbjuliane6@gmail.com>
2022-05-17 09:21:04 -0300

    fix bug

--- exercicio2/ex2back.php
+++ exercicio2/ex2back.php
@@ -12,7 +12,7 @@ function divisible($number){
     else if ($number % 5 == 0 && $number % 2 == 0){
         echo "$number is divisible by 5 and 2!";
     }  
-    if($number % 10 == 0) {
+    else if($number % 10 == 0) {
         echo "$number  is divisible by 10!";
     }
     else if($number % 5 == 0){
