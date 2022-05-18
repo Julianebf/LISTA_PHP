81a6c11e73609d20a31c32f901c244c7f781f190
Juliane Bezerra <fbjuliane6@gmail.com>
2022-05-18 20:53:53 -0300

    corrigir conflito

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <body>
     <div class="menu">
         <ul>
-            <li><a href="/exercicio1/index1.html">Exercicio 1</a></li>
+            <li><a href="/exercicio1/index.php">Exercicio 1</a></li>
 
             <li><a href="/exercicio2/ex2.php">Exercicio 2</a></li>
 
@@ -19,7 +19,13 @@
 
             <li><a href="/exercicio5/index.php">Exercicio 5</a></li>
 
-            <li><a href="/exercicio6/index.php">Exercicio 6</a></li>          
+            <li><a href="/exercicio6/index.php">Exercicio 6</a></li>  
+            
+            <li><a href="/exercicio7/index.php">Exercicio 7</a></li>  
+
+            <li><a href="/exercicio8/index.php">Exercicio 8</a></li>  
+
+            <li><a href="/exercicio9/index.php">Exercicio 9</a></li>  
         </ul>
     </div>
 </body>
