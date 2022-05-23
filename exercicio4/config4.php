139316f4959a627db73048b0bb297e0614933036
Juliane Bezerra <fbjuliane6@gmail.com>
2022-05-22 22:12:06 -0300

    corrigindo parte lógica

--- exercicio4/config4.php
+++ exercicio4/config4.php
@@ -1,28 +1,25 @@
 <?php
-    if(isset($_POST['verify'])){
-        $number1 = strip_tags(filter_input(INPUT_POST, 'number1'));
-        $number2 = strip_tags(filter_input(INPUT_POST, 'number2'));
-        $number3 = strip_tags(filter_input(INPUT_POST, 'number3'));           
+    if(isset($_POST['verificar'])){
+        $number1 = (filter_input(INPUT_POST, 'number1'));
+        $number2 = (filter_input(INPUT_POST, 'number2'));
+        $number3 = (filter_input(INPUT_POST, 'number3'));           
     }
-    if (($number1<$number2) && ($number2<$number3)) {
+    if (($number1<$number2) && ($number2<$number3) && ($number1<$number3)) {
         echo "Em ordem descrecente: $number3, $number2, $number1";
     }
-    elseif (($number1<$number3) && ($number3<$number2)) {
+    elseif (($number1<$number3) && ($number3<$number2) && ($number1<$number2)) {
         echo "Em ordem decrescente: $number2, $number3, $number1";
     }
-    elseif (($number2<$number1) && ($number1<$number3)) {
+    elseif (($number2<$number1) && ($number1<$number3) && ($number2<$number3)) {
         echo "Em ordem decrescente: $number3, $number1, $number2";
     }
-    elseif (($number2<$number3) && ($number3<$number1)) {
+    elseif (($number2<$number3) && ($number3<$number1) && ($number2<$number1)) {
         echo "Em ordem decrescente: $number1, $number3, $number2";
     }
-    elseif (($number3<$number1) && ($number1<$number2)) {
-        echo "Em ordem decrescente: $number1, $number3, $number2";
-    }
-    elseif (($number2<$number1) && ($number3<$number2)) {
+    elseif (($number2<$number1) && ($number3<$number2) && ($number3<$number1)){
         echo "Em ordem decrescente: $number1, $number2, $number3";
     }
-    elseif (($number1<$number2) && ($number3<$number2)) {
+    elseif (($number1<$number2) && ($number3<$number2) && ($number3<$number1)) {
         echo "Em ordem decrescente: $number2, $number1,$number3";
     }
 ?>
