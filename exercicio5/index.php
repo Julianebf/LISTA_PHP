e5fdec1081a4950c05d29609a2a9c99849dea211
Juliane Bezerra <fbjuliane6@gmail.com>
2022-05-22 22:56:51 -0300

    escrevendo html do exercicio5

---
+++ exercicio5/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Execicio5</title>
+</head>
+<body>
+Ler 3 números, os possíveis lados de um triângulo, e imprimir a classificação de 
+acordo com tamanho dos lados.
+    <form  method="post">
+        <div class="form">
+        <h3></h3>
+        <input type="int" placeholder="Primeiro numero" name="number1" id="number1">
+        <br/> <br/>
+        <input type="int" placeholder="Segundo numero" name="number2" id="number2">
+        <br/> <br/>
+        <input type="int" placeholder="Terceiro numero" name="number3" id="number3">
+        <br/> <br/>  
+        <input type="submit" name="verificar" value="Verificar">     
+        </div>
+    </form>
+</body>
+</html>
