c56763e3adafe0063ed567ebfde01d120f67c85b
Juliane Bezerra <fbjuliane6@gmail.com>
2022-05-30 22:00:00 -0300

    add html e css do exercicio 6

---
+++ exercicio6/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="/exercicio6/style.css">
+    <title>Execicio 6</title>
+</head>
+<body>
+Ler um número inteiro entre 1 e 12 e escrever o mês correspondente. Caso o número seja 
+fora desse intervalo, informar que não existe mês com este número.
+    <form  method="post">
+        <h2>Digite um numero de 1 á 12:</h2>
+		<input type="int" placeholder="digite o numero" name="valor" id="valor">
+		<br/>
+		<button name="verificar">verificar</button> 
+    </form> 
+</body>
+</html>
