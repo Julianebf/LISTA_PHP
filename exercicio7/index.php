aae4ac35513819a7fd5c71009a2da607368c4f73
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-06-06 10:17:42 -0300

    acrescentandolink com css e modificando alguns itens do css

--- exercicio7/index.php
+++ exercicio7/index.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="/exercicio7/style.css">
     <title>Execicio7</title>
 </head>
 <body>
-A biblioteca de uma universidade deseja fazer um algoritmo que leia o nome do livro que
+<p>A biblioteca de uma universidade deseja fazer um algoritmo que leia o nome do livro que
 será emprestado, o tipo de usuário (professor ou aluno) e possa imprimir um recibo
 conforme mostrado a seguir. Considerar que o professor tem 10 dias para devolver o livro
-o aluno somente 3 dias.
+o aluno somente 3 dias.</p></br><hr></br>
     <form method="post">
         <label> Livro:
             <input type ="text" placeholder="digite o nome do livro" name = "livro" id="texto1">
