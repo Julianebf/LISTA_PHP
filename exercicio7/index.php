d9712be9b425a9d6d4987f3706b3f6fa8206c8f8
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-06-12 14:02:57 -0300

    implementando o  código php no formulario, acrescentando alguns itens no css.

--- exercicio7/index.php
+++ exercicio7/index.php
@@ -13,19 +13,43 @@ será emprestado, o tipo de usuário (professor ou aluno) e possa imprimir um re
 conforme mostrado a seguir. Considerar que o professor tem 10 dias para devolver o livro
 o aluno somente 3 dias.</p></br><hr></br>
     <form method="post">
+        <label> Nome:
+            <input type ="text" placeholder="digite o seu nome" name = "nome" id= "nome"></br>
+        <label> 
         <label> Livro:
             <input type ="text" placeholder="digite o nome do livro" name = "livro" id="texto1">
-        </label>  
+        </label>        
         </br> 
         <label> Usuário:
             <select name = usuario id= usuario required>
-                <option value = "professor"> Professor</option>
-                <option value = "aluno"> Aluno</option>
+                <option value = ""></option>
+                <option value = "professor"> Professor(a)</option>
+                <option value = "aluno"> Aluno(a)</option>
+                <option value = "outros"> Outros </option>
             </select>  
         </label>
         </br>
         <button name="enviar">Enviar</button> 
         </br>
     </form>
+    <?php
+        if(isset ($_POST['enviar'])){
+            $nome = strip_tags(filter_input(INPUT_POST,'nome'));
+            $livro = strip_tags(filter_input(INPUT_POST, 'livro'));
+            $usuario = strip_tags(filter_input(INPUT_POST,'usuario'));
+
+            switch($usuario){
+                case "professor":
+                    echo "Professor:$nome </br> Livro:$livro </br> Prazo: Devolver o livro em 10 dias!";
+                    break;
+                case "aluno":
+                    echo "Aluno: $nome </br> Livro: $livro </br> Prazo: Devolver o livro em 3 dias!";
+                    break;
+                default:
+                    echo "O usuario não se enquadra no perfil de usuarios da biblioteca!";
+                    break;
+            }
+        }
+    ?>
 </body>
 </html>
