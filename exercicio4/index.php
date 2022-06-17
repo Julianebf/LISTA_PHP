c052c4721ed9a3dbb548738adee825c9dc3709d2
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-06-17 08:44:05 -0300

    mudando nome do arquivo

--- exercicio4/index.php
+++ exercicio4/index.php
@@ -9,6 +9,7 @@
 </head>
 <body>
 Entrar com 3 números e imprimi-los em ordem decrescente (suponha números diferentes)
+<<<<<<< HEAD
     <form action = "/exercicio4/index.php" method="post">
         <div class="form">
         <input type="int" placeholder="Escreva o primeiro valor" name="numero1" id="numero1">
@@ -46,6 +47,45 @@ Entrar com 3 números e imprimi-los em ordem decrescente (suponha números difer
             else {
                 echo "Informe numeros diferentes";
             }
+=======
+    <form  method="post">
+        <div class="form">
+        <h3>Order</h3>
+        <input type="int" placeholder="Enter number1" name="number1" id="number1">
+        <br/> <br/>
+        <input type="int" placeholder="Enter number2" name="number2" id="number2">
+        <br/> <br/>
+        <input type="int" placeholder="Enter number3" name="number3" id="number3">
+        <br/> <br/>
+        <button name="verificar">verificar</button>
+        </div>
+    </form>
+
+    <?php
+        if(isset($_POST['verificar'])){
+            $number1 = strip_tags(filter_input(INPUT_POST, 'number1', FILTER_SANITIZE_STRIPPED));
+            $number2 = strip_tags(filter_input(INPUT_POST, 'number2', FILTER_SANITIZE_STRIPPED));
+            $number3 = strip_tags(filter_input(INPUT_POST, 'number3', FILTER_SANITIZE_STRIPPED));
+            if (($number1<$number2) && ($number2<$number3) && ($number1<$number3)) {
+                echo "Em ordem descrecente: $number3, $number2, $number1";
+            }
+            elseif (($number1<$number3) && ($number3<$number2) && ($number1<$number2)) {
+                echo "Em ordem decrescente: $number2, $number3, $number1";
+            }
+            elseif (($number2<$number1) && ($number1<$number3) && ($number2<$number3)) {
+                echo "Em ordem decrescente: $number3, $number1, $number2";
+            }
+            elseif (($number2<$number3) && ($number3<$number1) && ($number2<$number1)) {
+               echo "Em ordem decrescente: $number1, $number3, $number2";
+            }
+            elseif (($number2<$number1) && ($number3<$number2) && ($number3<$number1)){
+                echo "Em ordem decrescente: $number1, $number2, $number3";
+            }
+            elseif (($number1<$number2) && ($number3<$number2) && ($number3<$number1)) {
+               echo "Em ordem decrescente: $number2, $number1,$number3";
+            }
+    
+>>>>>>> mudando nome do arquivo
         }
     ?>
 </body>
