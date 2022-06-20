baaa2a6d9a80a2ebe225d032dff8c7e48f34db4a
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-06-20 20:45:12 -0300

    adicionando parte logica do formulario

--- exercicio8/index.php
+++ exercicio8/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href="/exercicio8/style.css">
+    <link rel = "stylesheet" href="style.css">
     <title>Exercicio 8</title>
 </head>
 <body>
@@ -13,5 +13,22 @@
         <input type = "int" placeholder = "Digite um numero" name = "numero" id = numero>  </input> <br>
         <button name = enviar>Enviar</button>
     </form>
+    <?php
+    if(isset($_POST['enviar'])){
+        $numero = strip_tags(filter_input(INPUT_POST,'numero'));
+    }
+        for ($i=1; $i <= $numero; $i++){
+            echo " $i ";
+        } 
+        function produto($numero){
+            $cont =1;
+            for($j=1; $j <=$numero; $j++){
+                $cont *=$j;
+            }
+            return $cont;
+        }
+        $result = produto($numero);
+            echo  " = $result";
+    ?>
 </body>
 </html>
