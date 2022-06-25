d62e78a8c471a023afab6af668947f2ce6343eda
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-06-24 22:06:47 -0300

    acrescentando modificação no exercicio4

--- tests/acceptance/Exercicio4Cest.php
+++ tests/acceptance/Exercicio4Cest.php
@@ -11,7 +11,7 @@ class Exercicio4Cest
     public function ordemDecrescente(AcceptanceTester $I)
     {
         $I->amOnPage('/exercicio4');
-        $I->see('Entrar com 3 números e imprimi-los em ordem decrescente (suponha números diferentes)');
+        $I->see('Exercicio 4');
         $I->fillField('number1',0);
         $I->fillField('number2',0);
         $I->fillField('number3',0);
