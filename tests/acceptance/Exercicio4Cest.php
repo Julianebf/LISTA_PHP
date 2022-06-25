c61499183cd4b222d8d412263ad12dbd3a19d3c4
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-06-24 22:41:51 -0300

    corrigindo testes 4 e 5

--- tests/acceptance/Exercicio4Cest.php
+++ tests/acceptance/Exercicio4Cest.php
@@ -12,11 +12,20 @@ class Exercicio4Cest
     {
         $I->amOnPage('/exercicio4');
         $I->see('Exercicio 4');
-        $I->fillField('number1',0);
-        $I->fillField('number2',0);
-        $I->fillField('number3',0);
+        $I->fillField('numero1',9);
+        $I->fillField('numero2',9);
+        $I->fillField('numero3',9);
         $I->click('verificar');
-        $I->see('Informe');
-     
+        $I->see('Informe numeros diferentes');
+
+        $I->amOnPage('/exercicio4');
+        $I->see('Exercicio 4');
+        $I->fillField('numero1',12);
+        $I->fillField('numero2',25);
+        $I->fillField('numero3',13);
+        $I->click('verificar');
+        $I->see('Em ordem decrescente: 25, 13, 12');
+
+        
     }
 }
--- tests/acceptance/Exercicio5Cest.php
+++ tests/acceptance/Exercicio5Cest.php
@@ -14,5 +14,22 @@ class Exercicioa5Cest
         $I->fillField('numero3','9');
         $I->click('verificar');    
         $I->see('Equilatero');
+
+        $I->amOnPage('/exercicio5');
+        $I->see('Exercicio 5');
+        $I->fillField('numero1','7');
+        $I->fillField('numero2','6');
+        $I->fillField('numero3','5');
+        $I->click('verificar');    
+        $I->see('Escaleno');
+
+        $I->amOnPage('/exercicio5');
+        $I->see('Exercicio 5');
+        $I->fillField('numero1','5');
+        $I->fillField('numero2','15');
+        $I->fillField('numero3','5');
+        $I->click('verificar');    
+        $I->see('Isosceles');
+
     }
 }
