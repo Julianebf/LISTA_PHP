179e138e199c634b40c654f0e71b726aad915781
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-06-29 12:50:09 -0300

    adiionando html e css

---
+++ exercicio8/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel = "stylesheet" href="style.css">
+   <title>Exercicio 8</title>
+</head>
+<body>
+   <p>Criar um algoritmos que leia um número e imprima todos os número de 1 até ele e o seu produto.</p> <hr> </br>
+   <form method = "post">
+       <input type = "int" placeholder = "Digite um numero" name = "numero" id = numero>  </input> <br>
+       <button name = enviar>Enviar</button>
+   </form>
+</body>
+</html>
