e02603f9c281344bdffdd598bdcf86b1ed1a6db1
Juliane Bezerra Ferreira de Souza <fbjuliane6@gmail.com>
2022-07-03 13:31:16 -0300

    eovendo linha se sentido do teste

--- tests/acceptance/Exercicio10FCest.php
+++ tests/acceptance/Exercicio10FCest.php
@@ -6,11 +6,10 @@ class Exercicio10FCest
      * @test
      */
 
-    public function produtoDoNumero (AcceptanceTester $I)
+    public function frontexercicio10 (AcceptanceTester $I)
     {
         $I->amOnPage('/exercicio10');
         $I->see('Exercicio 10');
-        $I->fillField('input name', numeros);
         $I->click('verificar');    
     }
 }
